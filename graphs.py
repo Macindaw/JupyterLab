b35118786ff3957f579c78c2569acddea2b8e1a1
Macindaw <aidansouthwell2000s@gmail.com>
2021-12-13 17:08:34 +0800

    Added Pie Charts and Optimised Bar Chart Code

--- graphs.py
+++ graphs.py
@@ -1,13 +1,13 @@
 from matplotlib import pyplot as plt
 import numpy as np
-import pandas as pd
 
 class Graphs():
     
     def __init__(self):
         pass
     
-    def graph_bar(self, title, title_x, title_y, data_x, data_y, label_y):
+    def graph_bar(self, title, title_x, title_y, data_x, label_y):
+        data_y = np.arange(len(data_x))
         fig, ax = plt.subplots()
         hbars = ax.barh(data_y, data_x, align='center')
 
@@ -20,5 +20,21 @@ class Graphs():
 
         for i in range(len(data_y)): #Adding Labels to the Graph
           plt.text(data_x[i]/2, i, data_x[i], ha='center', va = 'center', color='white', fontweight='bold')
+
+        plt.show()
+
+    def graph_pie(self, title, data, labels):
+        total = np.sum(data)
+        if total > 1: #Check if data is in percent range
+            for i in range(len(data)):
+                data[i] = data[i] / total * 100
+        fig1, ax = plt.subplots()
+
+        text_args = dict(fontsize=10, weight='bold', color='white')  # Must use a dict to add text args
+        # For usefull information about labelling https://stackoverflow.com/questions/53239733/matplotlib-move-labels-into-middle-of-pie-chart
+        # https://matplotlib.org/stable/gallery/pie_and_polar_charts/pie_and_donut_labels.html#sphx-glr-gallery-pie-and-polar-charts-pie-and-donut-labels-py
+
+        ax.pie(data, labels=labels, shadow=True, autopct='%1.1f%%', textprops=text_args)
+        ax.set_title(title, bbox={'facecolor':'0.8', 'pad':5})
 
         plt.show()
